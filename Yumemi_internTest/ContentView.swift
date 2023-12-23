df66de94120d5f2984d4401ce5f0cf185add8fe8
Sawaki Shuto <swkshuto0208@icloud.com>
2023-12-23 23:52:28 +0900

    変更
    テストでAPI通信をできるように変更

--- Yumemi_internTest/ContentView.swift
+++ Yumemi_internTest/ContentView.swift
@@ -20,72 +20,72 @@ struct ContentView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
-
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+        VStack {
+            Text("名前を入力してください")
+            TextField("しゅうと", text: $name)
+            Text("血液型を入力してください")
+            TextField("しゅうと", text: $blood_type)
+            Text("生まれた年を入力してください")
+            TextField("", value: $year, format: .number)
+                .keyboardType(.numberPad)
+            Text("生まれた月を入力してください")
+            TextField("", value: $month, format: .number)
+                .keyboardType(.numberPad)
+            Text("生まれた日を入力してください")
+            TextField("", value: $day, format: .number)
+                .keyboardType(.numberPad)
+            Button(action: {
+                ViewModel.executionFortune(name: name,
+                                           birthday: YearMonthDay(
+                                            year: year,
+                                            month: month,
+                                            day: day
+                                           ),
+                                           blood_type: blood_type,
+                                           today: YearMonthDay(year: 2021, month: 12, day: 12)
+                )
+            }, label: {
+                Text("送信")
+            })
         }
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+    //    private func addItem() {
+    //        withAnimation {
+    //            let newItem = Item(context: viewContext)
+    //            newItem.timestamp = Date()
+    //
+    //            do {
+    //                try viewContext.save()
+    //            } catch {
+    //                let nsError = error as NSError
+    //                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+    //            }
+    //        }
+    //    }
 
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+    //    private func deleteItems(offsets: IndexSet) {
+    //        withAnimation {
+    //            offsets.map { items[$0] }.forEach(viewContext.delete)
+    //
+    //            do {
+    //                try viewContext.save()
+    //            } catch {
+    //                let nsError = error as NSError
+    //                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+    //            }
+    //        }
+    //    }
+    //}
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+    //private let itemFormatter: DateFormatter = {
+    //    let formatter = DateFormatter()
+    //    formatter.dateStyle = .short
+    //    formatter.timeStyle = .medium
+    //    return formatter
+    //}()
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
 }
