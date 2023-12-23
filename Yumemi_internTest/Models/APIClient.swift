a550d085ae53c6facc4c39b82f7aa17ed0b6e5c8
Sawaki Shuto <swkshuto0208@icloud.com>
2023-12-23 11:42:54 +0900

    Startsession追加、request生成まで完了

--- Yumemi_internTest/Models/APIClient.swift
+++ Yumemi_internTest/Models/APIClient.swift
@@ -7,4 +7,33 @@
 
 import Foundation
 
+struct APIClient {
+    private let baseURL = URL(string: "https://yumemi-ios-junior-engineer-codecheck.app.swift.cloud")!
+    func StartSession(name: String, birthday: YearMonthDay, blood_type: String, today: YearMonthDay) async -> Result<APIResponse, APIClientError> {
 
+        let urlComponents = URLComponents(
+            url: baseURL.appending(path: "/my_fortune"),
+            resolvingAgainstBaseURL: true
+        )
+
+        guard let url = urlComponents?.url else {
+            return .failure()
+        }
+        let body = try? JSONSerialization.data(withJSONObject: [
+            "name": name,
+            "birthDay": birthday,
+            "blood_type": blood_type,
+            "today": today
+        ])
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = body
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.allHTTPHeaderFields = ["API-Version": "v1"]
+
+
+
+    }
+
+}
