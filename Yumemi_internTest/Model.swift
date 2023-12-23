c0dcdf065c3f755e8db98219802f57067f65553f
Sawaki Shuto <swkshuto0208@icloud.com>
2023-12-23 10:14:41 +0900

    APIResponseInfo,MonthDay作成完了

--- Yumemi_internTest/Model.swift
+++ Yumemi_internTest/Model.swift
@@ -22,7 +22,15 @@ struct YearMonthDay: Codable {
 struct APIResponseInfo: Identifiable, Decodable {
     let id: Int
     let name: String
-    let 
+    let capital: String
+    let citizen_day: MonthDay?
+    let has_coast_line: Bool
+    let logo_url: String
+    let brief: String
+}
 
+struct MonthDay: Decodable {
+    let month: Int
+    let day: Int
 }
 
