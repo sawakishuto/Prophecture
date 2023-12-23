c9fb7ecd160106980bd8f91fb9b85774885b24d2
Sawaki Shuto <swkshuto0208@icloud.com>
2023-12-23 10:09:16 +0900

    create
    Model.swift

--- Yumemi_internTest/Model.swift
+++ Yumemi_internTest/Model.swift
@@ -5,3 +5,24 @@
 //  Created by 澤木柊斗 on 2023/12/23.
 //
 
+import Foundation
+
+struct APIPostInfo: Codable {
+    var id: String = UUID().uuidString
+    let name: String
+    let birthday: YearMonthDay
+}
+
+struct YearMonthDay: Codable {
+    let year: Int
+    let month: Int
+    let day: Int
+}
+
+struct APIResponseInfo: Identifiable, Decodable {
+    let id: Int
+    let name: String
+    let 
+
+}
+
