5f520ea82249514d8e8a72e268d93911cb014ebd
Sawaki Shuto <swkshuto0208@icloud.com>
2023-12-26 10:31:13 +0900

    初期画面の追加

---
+++ Yumemi_internTest/FirstPageView.swift
@@ -0,0 +1,18 @@
+//
+//  FirstPageView.swift
+//  Yumemi_internTest
+//
+//  Created by 澤木柊斗 on 2023/12/26.
+//
+
+import SwiftUI
+
+struct FirstPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstPageView()
+}
