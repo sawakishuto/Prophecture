ca8d63f82bccbac9c2c13624ae3e6ab7679f46be
Sawaki Shuto <swkshuto0208@icloud.com>
2023-12-26 14:34:52 +0900

    画面全体にアニメーションを追加

--- Yumemi_internTest/Views/FirstPageView.swift
+++ Yumemi_internTest/Views/FirstPageView.swift
@@ -22,35 +22,50 @@ struct FirstPageView: View {
                 .resizable()
                 .scaledToFill()
                 .ignoresSafeArea()
-
-            Image("firstViewTitleImage")
-                .resizable()
-                .scaledToFit()
-                .padding(.bottom, 450)
-                .scaleEffect(isShowtitle ? 0.8 : 0.0)
-                .animation(.spring(duration: 2.4, bounce: 0.45), value: isShowtitle)
-
-            Image("titleCommentImage")
-                .resizable()
-                .scaledToFit()
-                .scaleEffect(0.7)
-                .padding(EdgeInsets(top: 0, leading: 240, bottom: 550, trailing: 0))
+                .scaleEffect(expandImage ? 7.0 : 1.0)
+                .animation(.spring(duration: 2.7), value: expandImage)
 
 
-            Text("Tap To Start!")
-                .fontWeight(.black)
-                .font(.system(size: 35))
-                .padding(.top, 300)
-                .scaleEffect(isShowtitle ? 1.0 : 0.8)
-                .animation(.spring(duration: 1.0).repeatForever(), value: bounceMessage)
+            ZStack {
+
+                Image("firstViewTitleImage")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.bottom, 450)
+                    .scaleEffect(isShowTitle ? 0.8 : 0.0)
+                    .animation(.spring(duration: 2.4, bounce: 0.45), value: isShowTitle)
+
+                Image("titleCommentImage")
+                    .resizable()
+                    .scaledToFit()
+                    .scaleEffect(0.7)
+                    .padding(EdgeInsets(top: 0, leading: 240, bottom: 550, trailing: 0))
+
+
+                Text("Tap To Start!")
+                    .fontWeight(.black)
+                    .font(.system(size: 35))
+                    .padding(.top, 300)
+                    .scaleEffect(isShowTitle ? 1.0 : 0.8)
+                    .animation(.spring(duration: 1.0).repeatForever(), value: bounceMessage)
+                    }
+            .scaleEffect(isClose ? 1.0: 0.0)
+            .animation(.spring(duration: 0.5), value: !isClose)
 
         }
+
         .onAppear{
-            isShowtitle.toggle()
+            isShowTitle.toggle()
             bounceMessage.toggle()
         }
         .onTapGesture {
+            isClose.toggle()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                expandImage.toggle()
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.4) {
                 self.ViewType.viewType = .tabPage
+            }
             }
 
     }
