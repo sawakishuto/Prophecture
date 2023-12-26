c874bf3b1f473ef139a44cb053a8a821800b4570
Sawaki Shuto <swkshuto0208@icloud.com>
2023-12-26 17:08:34 +0900

    画像追加

--- Yumemi_internTest/Views/menuView.swift
+++ Yumemi_internTest/Views/menuView.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct menuView: View {
+    let ViewType: ViewTypeModel
+    @State private var opacity: Double = 0.0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("menuImage")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+                .onTapGesture {
+                    ViewType.viewType = .tabPage
+                }
+        }
+            .opacity(self.opacity)
+            .onAppear {
+                withAnimation(.linear(duration: 1.0)) {
+                               self.opacity = 1.0
+                           }
+            }
     }
-}
-
-#Preview {
-    menuView()
 }
