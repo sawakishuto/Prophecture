f88cfaccc4222c3d38f8dad4adfa803ce599c33c
Sawaki Shuto <swkshuto0208@icloud.com>
2023-12-26 11:52:49 +0900

    viewTypeModelクラスを作成、View間で画面遷移をできるようにしたい

---
+++ Yumemi_internTest/ViewTypeModel.swift
@@ -0,0 +1,12 @@
+//
+//  ViewTypeModel.swift
+//  Yumemi_internTest
+//
+//  Created by 澤木柊斗 on 2023/12/26.
+//
+
+import Foundation
+
+final class ViewTypeModel: ObservableObject {
+    @Published var viewType: ViewType = .firstPage
+}
