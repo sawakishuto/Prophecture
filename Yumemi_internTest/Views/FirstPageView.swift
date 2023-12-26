281b82fcd80741ef516e5db9bd8c3875976fe05c
Sawaki Shuto <swkshuto0208@icloud.com>
2023-12-26 13:23:08 +0900

    FirstViewのレイアウト完成

--- Yumemi_internTest/Views/FirstPageView.swift
+++ Yumemi_internTest/Views/FirstPageView.swift
@@ -10,11 +10,32 @@ import SwiftUI
 struct FirstPageView: View {
     let ViewType: ViewTypeModel
     var body: some View {
-        Button(action: {
-            self.ViewType.viewType = .tabPage
-        }, label: {
-            Text("遷移")
-        })
+        ZStack {
+            Image("firstviewImage")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+
+            Image("firstViewTitleImage")
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(0.8)
+                .padding(.bottom, 450)
+
+            Image("titleCommentImage")
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(0.7)
+                .padding(EdgeInsets(top: 0, leading: 240, bottom: 550, trailing: 0))
+
+            Text("Tap To Start!")
+                .fontWeight(.black)
+                .font(.system(size: 35))
+                .padding(.top, 300)
+        }
+            .onTapGesture {
+                self.ViewType.viewType = .tabPage
+            }
 
     }
 }
