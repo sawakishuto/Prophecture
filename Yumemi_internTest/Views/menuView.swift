8847c7e77d420c3008459c5b2903abd449b1153a
Sawaki Shuto <swkshuto0208@icloud.com>
2023-12-26 16:29:16 +0900

    メニュー画面のためのmenuView追加

---
+++ Yumemi_internTest/Views/menuView.swift
@@ -0,0 +1,18 @@
+//
+//  menuView.swift
+//  Yumemi_internTest
+//
+//  Created by 澤木柊斗 on 2023/12/26.
+//
+
+import SwiftUI
+
+struct menuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    menuView()
+}
