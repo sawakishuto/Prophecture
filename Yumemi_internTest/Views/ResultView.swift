b35ccfba2f95eb2ed5f5ac4163aac29eec5bb470
Sawaki Shuto <swkshuto0208@icloud.com>
2023-12-28 15:02:28 +0900

    カードが出てくるようにした

---
+++ Yumemi_internTest/Views/ResultView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultView.swift
+//  Yumemi_internTest
+//
+//  Created by 澤木柊斗 on 2023/12/27.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultView()
+}
