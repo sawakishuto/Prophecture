07868d6eed65b8b11e08a928668e71ec986c02bd
Sawaki Shuto <swkshuto0208@icloud.com>
2023-12-30 23:37:21 +0900

    結果を表示するために必要な変数を追加

--- Yumemi_internTest/Views/ResultView.swift
+++ Yumemi_internTest/Views/ResultView.swift
@@ -6,8 +6,23 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ResultView: View {
+    @ObservedObject var ViewModel = FortuneViewModel()
+    let returnName: String
+    let returnCapital: String
+    let returnCitizen_day: MonthDay
+    let returnHas_coast_line: Bool
+    let returnLogo_url: String?
+    let returnBrief: String
+    private let CardAppear = try! AVAudioPlayer(data: NSDataAsset(name: "cardApear")!.data)
+    @State private var appearOffset: Double = 0.0
+    @State private var isappearA: Bool = false
+    @State private var isappearB: Bool = false
+    @State private var isSmallEffect: Bool = false
+    @State private var islargeEffect: Bool = false
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
