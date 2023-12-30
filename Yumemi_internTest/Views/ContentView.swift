e73437d2a24af4d3bf368a7c75a5513daab6e068
Sawaki Shuto <swkshuto0208@icloud.com>
2023-12-30 23:38:18 +0900

    占いをするページを作成

--- Yumemi_internTest/Views/ContentView.swift
+++ Yumemi_internTest/Views/ContentView.swift
@@ -48,120 +48,177 @@ struct ContentView: View {
         animation: .default)
     private var items: FetchedResults<Item>
     var body: some View {
-        ZStack {
-            VStack {
-                Text("名前を入力してください")
-                TextField("しゅうと", text: $name)
-
-                Text("血液型を入力してください")
-                Picker(selection: $blood_type, label: Text("")) {
-                    ForEach(Blood_type.allCases) { blood in
-                        Text("\(blood.blood_type)")
-                    }
-                }
-                    .pickerStyle(.menu)
-                    .padding(.top, 40)
-                    Text("生年月日を入力してください")
-                    HStack {
-                        Picker(selection: self.$year, label: Text("年")){
-                            ForEach(1950..<2025){ _x in
-                                Text("\(_x)")
-                            }
+        if !isResult {
+            ZStack {
+                mainView
+                .onChange(of: slideOffset) { newValue in
+                    if newValue > -400 {
+                        withAnimation(.easeInOut(duration: 0.7)) {
+                            self.slideOffset = -1000
+                            PaperMusic.play()
                         }
-                        Picker(selection: self.$month, label: Text("月")){
-                            ForEach(1..<12){ _x in
-                                Text("\(_x)")
-                            }
-                        }
-                        Picker(selection: self.$day, label: Text("日")){
-                            if self.month != 1 {
-                                ForEach(1..<32){ _x  in
-                                    Text("\(_x)")
-                                }
-                            } else {
-                                ForEach(1..<29){ _x in
-                                    Text("\(_x)")
-                                }
-                            }
-                        }
-                    }
-                    Button(action: {
-                        let currentDate: Date = Date()
-                        let calender = Calendar.current
-                        let calComponent = calender.dateComponents([
-                            Calendar.Component.year, Calendar.Component.month, Calendar.Component.day
-                        ], from: currentDate)
-
-                        ViewModel.executionFortune(
-                            name: name,
-                            birthday: YearMonthDay(
-                                year: year + 1950,
-                                month: month + 1,
-                                day: day + 1
-                            ),
-                            blood_type: blood_type.blood_type,
-                            today: YearMonthDay(
-                                year: calComponent.year ?? 1995,
-                                month: calComponent.month ?? 12,
-                                day: calComponent.day ?? 25
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                            ViewModel.executionFortune(
+                                name: name,
+                                birthday: YearMonthDay(
+                                    year: year + 1950,
+                                    month: month + 1,
+                                    day: day + 1
+                                ),
+                                blood_type: blood_type.blood_type,
+                                today: YearMonthDay(
+                                    year: getCurrentTime().year ?? 1995,
+                                    month: getCurrentTime().month ?? 12,
+                                    day: getCurrentTime().day ?? 25
+                                )
                             )
-                        )
-
-                        //                self.ViewType.viewType = .firstPage
-                    }, label: {
-                        Text("送信")
-                    })
-                    if ViewModel.fortuneResults?.logo_url != nil {
-                        AsyncImage(url: URL(string: (ViewModel.fortuneResults?.logo_url)!))
+                        }
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                            isResult = true
+                        }
                     }
                 }
-            .padding(.trailing, 30)
-            Image("kartenImage")
-                .resizable()
-                .offset(x: imgOffset)
-                .scaledToFill()
-                .ignoresSafeArea()
-                .scaleEffect(1.2)
-        }
-        .onAppear {
-            withAnimation(.easeInOut(duration: 0.7)) {
-                   self.imgOffset = -UIScreen.main.bounds.width * 2
-                 }
-        }
-        }
+                Image("kartenImage")
+                    .resizable()
+                    .scaleEffect(1.2)
+                    .offset(x: imgOffset)
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                    .padding(.top, 25)
 
-        //    private func addItem() {
-        //        withAnimation {
-        //            let newItem = Item(context: viewContext)
-        //            newItem.timestamp = Date()
-        //
-        //            do {
-        //                try viewContext.save()
-        //            } catch {
-        //                let nsError = error as NSError
-        //                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        //            }
-        //        }
-        //    }
-
-        //    private func deleteItems(offsets: IndexSet) {
-        //        withAnimation {
-        //            offsets.map { items[$0] }.forEach(viewContext.delete)
-        //
-        //            do {
-        //                try viewContext.save()
-        //            } catch {
-        //                let nsError = error as NSError
-        //                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        //            }
-        //        }
-        //    }
-        //}
-
-        //private let itemFormatter: DateFormatter = {
-        //    let formatter = DateFormatter()
-        //    formatter.dateStyle = .short
-        //    formatter.timeStyle = .medium
-        //    return formatter
-        //}()
+            }
+            .onAppear {
+                withAnimation(.easeInOut(duration: 0.7)) {
+                    self.imgOffset = -UIScreen.main.bounds.width * 2
+                }
+        }
+        } else {
+            ResultView(
+                returnName: self.ViewModel.fortuneResults?.name ?? "滋賀県",
+                returnCapital: self.ViewModel.fortuneResults?.capital ?? "大津市",
+                returnCitizen_day: self.ViewModel.fortuneResults?.citizen_day ?? MonthDay(month: 11, day: 11),
+                returnHas_coast_line: self.ViewModel.fortuneResults?.has_coast_line ?? false,
+                returnLogo_url: self.ViewModel.fortuneResults?.logo_url,
+                returnBrief: self.ViewModel.fortuneResults?.brief ?? "特になし",
+                ViewType: ViewType
+            )
+        }
     }
+}
+
+private extension ContentView {
+    var mainView: some View {
+        ZStack {
+            Image("karteImage")
+                .resizable()
+                .scaledToFit()
+                .padding(.leading, 40)
+
+            TextField("田中太郎", text: $name)
+                .frame(width: 150, height: 35, alignment: .center)
+                .background(.white)
+                .cornerRadius(10)
+                .padding(.bottom, 250)
+
+            Picker(selection: $blood_type, label: Text("")) {
+                ForEach(Blood_type.allCases) { blood in
+                    Text("\(blood.blood_type)")
+                }
+            }
+            .pickerStyle(.menu)
+            .padding(.top, 240)
+            birthday
+                .frame(width: 300)
+                .padding(.leading, 50)
+                .fontWeight(.black)
+        }
+        .padding(.trailing, 30)
+        .offset(y: slideOffset)
+        .gesture(
+            DragGesture().onChanged { value in
+                self.slideOffset = value.translation.height
+            }
+        )
+    }
+    var birthday: some View {
+        HStack {
+            Picker(selection: self.$year, label: Text("年")){
+                ForEach(1950..<2025){ _x in
+                    Text("\(_x)")
+                }
+            }
+            .foregroundStyle(.black)
+            Picker(selection: self.$month, label: Text("月")){
+                ForEach(1..<12){ _x in
+                    Text("\(_x)")
+                }
+            }
+            .frame(width: 100)
+            .foregroundStyle(.black)
+            Picker(selection: self.$day, label: Text("日")){
+                if self.month != 1 {
+                    ForEach(1..<32){ _x  in
+                        Text("\(_x)")
+                    }
+                } else {
+                    ForEach(1..<29){ _x in
+                        Text("\(_x)")
+                    }
+                }
+            }
+        }
+    }
+    func getCurrentTime() -> DateComponents {
+        let currentDate: Date = Date()
+        let calender = Calendar.current
+        let calComponent = calender.dateComponents([
+            Calendar.Component.year,
+            Calendar.Component.month,
+            Calendar.Component.day
+        ], from: currentDate)
+        return calComponent
+    }
+}
+
+
+
+
+
+
+
+
+//    private func addItem() {
+//        withAnimation {
+//            let newItem = Item(context: viewContext)
+//            newItem.timestamp = Date()
+//
+//            do {
+//                try viewContext.save()
+//            } catch {
+//                let nsError = error as NSError
+//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//            }
+//        }
+//    }
+
+//    private func deleteItems(offsets: IndexSet) {
+//        withAnimation {
+//            offsets.map { items[$0] }.forEach(viewContext.delete)
+//
+//            do {
+//                try viewContext.save()
+//            } catch {
+//                let nsError = error as NSError
+//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//            }
+//        }
+//    }
+//}
+
+//private let itemFormatter: DateFormatter = {
+//    let formatter = DateFormatter()
+//    formatter.dateStyle = .short
+//    formatter.timeStyle = .medium
+//    return formatter
+//}()
+
