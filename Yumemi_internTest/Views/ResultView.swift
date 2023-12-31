54a3bd090025761a6c3892f45eec2e82a25e727b
Sawaki Shuto <swkshuto0208@icloud.com>
2023-12-31 14:03:05 +0900

    状態変化を定義して挙動変更

--- Yumemi_internTest/Views/ResultView.swift
+++ Yumemi_internTest/Views/ResultView.swift
@@ -9,6 +9,41 @@ import SwiftUI
 import AVFoundation
 
 struct ResultView: View {
+    enum cardState {
+        case normal
+        case large
+        case small
+        var UpcardSituation: Double {
+            switch self {
+            case .normal: return 1.0
+            case .large: return 1.5
+            case .small: return 0.7
+
+            }
+        }
+        var downcardSituation: Double {
+            switch self {
+            case .normal: return 1.0
+            case .large: return 0.7
+            case .small: return 1.5
+            }
+        }
+        var upCardPlusOffset: CGFloat {
+            switch self {
+            case .normal: return 0
+            case .large: return 200
+            case .small: return 200
+            }
+        }
+        var downCardPlusOffset: CGFloat {
+            switch self {
+            case .normal: return 0
+            case .large: return -300
+            case .small: return -300
+            }
+        }
+    }
+
     @ObservedObject var ViewModel = FortuneViewModel()
     let returnName: String
     let returnCapital: String
@@ -22,29 +57,67 @@ struct ResultView: View {
     @State private var isappearB: Bool = false
     @State private var isSmallEffect: Bool = false
     @State private var islargeEffect: Bool = false
-
-
+    @State private var upCardZ_Index: Double = 10
+    @State private var downCardZ_Index: Double = 10
+    @State private var upCardOffset: CGFloat = -2000
+    @State private var downCardOffset: CGFloat = -2000
+    @State private var cardStates: cardState = .normal
     let ViewType: ViewTypeModel
+    
     var body: some View {
-        ZStack {
-            CardBack()
-                .offset(y: isappearA ? -150: -2000)
+        GeometryReader { geometory in
 
-           CardBack()
-                .offset(y: isappearB ? 150: -2000)
+            ZStack(alignment: .center) {
+                Image("tableImage")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                    .frame(width: geometory.size.width, height: geometory.size.height)
+                VStack(alignment: .center, spacing: 150) {
+                    Image("cardImage")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200)
+                        .scaleEffect(cardStates.UpcardSituation)
+                        .offset(y: upCardOffset + cardStates.upCardPlusOffset)
+                        .zIndex(upCardZ_Index)
+                        .onTapGesture {
+                            cardStates = .large
+                            downCardZ_Index = 9
+                            upCardZ_Index = 10
+                        }
 
+                    Image("cardImage")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200)
+                        .scaleEffect(cardStates.downcardSituation)
+                        .offset(y: downCardOffset + cardStates.downCardPlusOffset)
+                        .zIndex(downCardZ_Index)
+                        .onTapGesture {
+                            cardStates = .small
+                            upCardZ_Index = 9
+                            downCardZ_Index = 10
+                        }
+                }
+            }
+            .onTapGesture {
+                cardStates = .normal
+            }
+            .animation(.easeInOut(duration: 0.7), value: upCardOffset)
+            .animation(.easeInOut(duration: 0.7), value: downCardOffset)
+            .animation(.easeInOut(duration: 0.3), value: cardStates)
+            .animation(.easeInOut(duration: 0.3), value: cardStates)
         }
-        .animation(.easeInOut(duration: 0.7), value: isappearA)
-        .animation(.easeInOut(duration: 0.7), value: isappearB)
         .onAppear {
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
                 CardAppear.play()
-                isappearA = true
+               upCardOffset = 0
             }
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.9) {
                 CardAppear.play()
                 isappearB = true
-
+                downCardOffset = 0
             }
         }
     }
