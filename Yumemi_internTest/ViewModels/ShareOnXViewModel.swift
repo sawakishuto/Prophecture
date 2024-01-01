f259799b99ed4b849229bb2ee63b90322e0fbbbd
Sawaki Shuto <swkshuto0208@icloud.com>
2024-01-02 00:49:47 +0900

    X投稿のためのVM追加

--- Yumemi_internTest/ViewModels/ShareOnXViewModel.swift
+++ Yumemi_internTest/ViewModels/ShareOnXViewModel.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import UIKit
+
+final class ShareOnXViewModel: ObservableObject {
+    func ShareOnX(name: String) {
+        let text = "占いの結果、\(name)が相性がいいらしい！！今度行ってみようかな〜！"
+        let hashTag = "#Prophecture\n#都道府県占い\n#占い"
+        let completedText = text + "\n" + hashTag
+        let encodedText = completedText.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        if let encodedText = encodedText, let url = URL(string: "https://twitter.com/intent/tweet?text=\(encodedText)") { UIApplication.shared.open(url) }
+    }
+}
