2834c0e9ff0400b71bd9f43871165c3c22e241bf
Sawaki Shuto <swkshuto0208@icloud.com>
2024-01-04 22:57:19 +0900

    ViewModel追加

--- Yumemi_internTest/ViewModels/CoreDataViewModel.swift
+++ Yumemi_internTest/ViewModels/CoreDataViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import CoreData
+final class CoreDataViewModel: ObservableObject {
+    let coreDataModel: CoreDataModel
+    init(coreDataModel: CoreDataModel) {
+        self.coreDataModel = coreDataModel
+    }
+    @Published var fortuneData:[ItemData]  = []
+
+    func saveData(name: String, timeStamp: Date, prefecture: String) {
+        coreDataModel.addData(name: name, timeStamp: Date(), prefecture: prefecture)
+    }
+    func fetchData() {
+        var fetchedData: [ItemData]  = []
+        let datas = coreDataModel.fetchData()
+        if !datas.isEmpty {
+            for data in datas {
+                fetchedData.append(ItemData(
+                    name: data.name ?? "Unknown",
+                    timeStamp: data.timestamp ?? Date(),
+                    prefecture: data.prefecture ?? "Unknown"
+                ))
+            }
+        }
+
+    }
+}
