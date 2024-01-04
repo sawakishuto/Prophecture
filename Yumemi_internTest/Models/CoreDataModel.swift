9d0aec8768137ddf77abb1e0842a401f12b9cfea
Sawaki Shuto <swkshuto0208@icloud.com>
2024-01-04 22:37:44 +0900

    Coredataを扱うためのViewModelとModelを追加

---
+++ Yumemi_internTest/Models/CoreDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataModel.swift
+//  Yumemi_internTest
+//
+//  Created by 澤木柊斗 on 2024/01/04.
+//
+
+import Foundation
---
+++ Yumemi_internTest/ViewModels/CoreDataViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataViewModel.swift
+//  Yumemi_internTest
+//
+//  Created by 澤木柊斗 on 2024/01/04.
+//
+
+import Foundation
