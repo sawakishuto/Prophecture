664b6b704063f24895acea0ebba13260c70e1a33
Sawaki Shuto <swkshuto0208@icloud.com>
2024-01-04 22:54:44 +0900

    データを扱うためのロジック追加

--- Yumemi_internTest/Models/CoreDataModel.swift
+++ Yumemi_internTest/Models/CoreDataModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import CoreData
+
+ struct CoreDataModel {
+     let context: NSManagedObjectContext
+    func addData(name: String, timeStamp: Date, prefecture: String) {
+        let newItem = Item(context: context)
+        newItem.name = name
+        newItem.timestamp = Date()
+        newItem.prefecture = prefecture
+        do {
+            try context.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+    }
+     func fetchData() -> [Item] {
+         let request = NSFetchRequest<Item>(entityName: "Item")
+         request.sortDescriptors = [NSSortDescriptor(key: "timestamp", ascending: false)]
+         do {
+              return try context.fetch(request)
+          } catch {
+              fatalError("取得失敗")
+          }
+     }
+}
