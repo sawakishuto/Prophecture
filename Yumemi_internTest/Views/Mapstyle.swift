32deb725811623900c10c1b983a8278d023cc7aa
Sawaki Shuto <swkshuto0208@icloud.com>
2024-01-05 01:48:45 +0900

    一覧表示の時のカード作成

--- Yumemi_internTest/Views/Mapstyle.swift
+++ Yumemi_internTest/Views/Mapstyle.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct Mapstyle: View {
+    let logoUrl: String
+    let prefecture: String
+    let timeStamp: String
+    let name: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+        AsyncImage(url: URL(string: logoUrl)) { image in
+            image.image?
+                .resizable()
+                .scaledToFit()
+                .frame(width: 60)
+        }
+            VStack(alignment: .leading) {
+
+                Text(timeStamp)
+                    .font(.caption2)
+                Text(name)
+                    .font(.headline)
+                    .fontWeight(.black)
+
+                HStack(spacing: 0) {
+                    Text("相性が良い")
+                    Text("都道府県")
+                        .foregroundStyle(.green)
+                        .padding(.trailing)
+                    Text(prefecture)
+                        .fontWeight(.black)
+                }
+            }
+    }
+        .padding(.vertical, 20)
+        .padding(.horizontal, 30)
+        .background(Color.white.cornerRadius(20))
+        .shadow(radius: 6)
+
     }
 }
 
-#Preview {
-    Mapstyle()
-}
+
