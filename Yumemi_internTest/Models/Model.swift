b1f5f6b87d2e25c48367707f863b6a910ea959ac
Sawaki Shuto <swkshuto0208@icloud.com>
2024-01-04 17:40:32 +0900

    順番とCodable追加

--- Yumemi_internTest/Models/Model.swift
+++ Yumemi_internTest/Models/Model.swift
@@ -20,16 +20,16 @@ struct YearMonthDay: Codable {
     let day: Int
 }
 
-struct APIResponseInfo: Decodable {
+struct APIResponseInfo: Codable {
     let name: String
-    let capital: String
-    let citizen_day: MonthDay?
     let has_coast_line: Bool
+    let citizen_day: MonthDay?
+    let capital: String
     let logo_url: String?
     let brief: String
 }
 
-struct MonthDay: Decodable {
+struct MonthDay: Codable {
     let month: Int
     let day: Int
     
